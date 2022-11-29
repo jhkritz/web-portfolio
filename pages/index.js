06c1482f77eb98d9cad5719c5d007604c0383c06
Kritzinger, JH, Mnr [21616442@sun.ac.za] <21616442@sun.ac.za>
2022-11-29 17:06:16 +0200

    Updating titles

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,13 @@ export default function App() {
   return (
     <div>
       <Blob></Blob>
+      <div className="heading">
+        <h1 className="title">Work, work, work.</h1>
+        <h2 className="subtitle">
+          Each project is unique, feel free to explore.
+        </h2>
+      </div>
+
       <ProductCard></ProductCard>
       <ProductCard></ProductCard>
       <ProductCard></ProductCard>
