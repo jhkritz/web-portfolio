196570eae827d8eba9ab2171de725e71b34b990c
Kritzinger, JH, Mnr [21616442@sun.ac.za] <21616442@sun.ac.za>
2022-12-01 15:27:28 +0200

    Added image styling to carousel

--- components/carousel.js
+++ components/carousel.js
@@ -2,7 +2,17 @@ import styles from "./carousel.module.css";
 
 export default function Carousel() {
   return (
-    <div>
+    <div class={styles["head"]}>
+      <div className={styles["heading"]}>
+        <h1 className={styles["maintitle"]}>
+          Some other design projects
+          <img
+            src="https://i.ibb.co/RPj22r0/notebook-dynamic-gradient.png"
+            className={styles["icon2"]}
+          ></img>
+        </h1>
+        <h2 className={styles["subtitle"]}></h2>
+      </div>
       <div class={styles["parent"]}>
         <div class={styles["slider"]}>
           <div class={styles["slide-track"]}>
